2bb71fdc600254ccf824000d95e0bb5f59a399c4
Si Beaumont <beaumont@apple.com>
2025-06-27 17:32:14 +0100

    Add tests to check bootstrap API does bootstrap subsystems (#226)
    ## Motivation
    
    As part of the roadmap for 1.0 (see #206), we're adding new APIs to
    bootstrap the Swift observability APIs (#205). These have been
    implemented in previous PRs, but it would be nice to test the side
    effect of the bootstrap.
    
    Historically this has been challenging, since bootstrapping these
    subsystems is a one-time, process-global operation, and attempting a
    second bootstrap would result in a fatal error from the underlying Swift
    observability API package.
    
    Thankfully, Swift Testing now has exit-tests, which can be used to run a
    test (or part of a test) in its own process. A primary motivation for
    that feature is testing for fatal errors, but it can also be used to
    validate that the forked process exited with success, which makes it a
    great fit for testing the side-effects of calling the `OTel.bootstrap`
    API: specifically, that it has indeed bootstrapped the underlying
    subsystem. Additionally, we'd can use a similar technique to confirm
    that the `make[Logging|Metrics|Tracing]Backend` APIs have _not_ resulted
    in a bootstrap.
    
    ## Modifications
    
    - Add tests for bootstrap APIs
    
    ## Result
    
    - Test coverage that `OTel.bootstrap` _has_ indeed caused a bootstrap
    - Test coverage that `OTel.make[Metrics|Tracing]Backend` has _not_
    caused a bootstrap

---
+++ Tests/OTelTests/BootstrapTests.swift
@@ -0,0 +1,75 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift OTel open source project
+//
+// Copyright (c) 2025 the Swift OTel project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#if compiler(>=6.2) // Swift Testing exit tests only added in 6.2
+import Metrics
+import OTel // NOTE: Not @testable import, to test public API visibility.
+import Testing
+import Tracing
+
+@Suite struct OTelBootstrapTests {
+    @Test func testMakeMetricsBackend() async throws {
+        await #expect(processExitsWith: .success, "Running in a separate process because test uses bootstrap") {
+            let (factory, _) = try OTel.makeMetricsBackend()
+            MetricsSystem.bootstrap(factory)
+        }
+    }
+
+    @Test func testMakeTracingBackend() async throws {
+        await #expect(processExitsWith: .success, "Running in a separate process because test uses bootstrap") {
+            let (factory, _) = try OTel.makeTracingBackend()
+            InstrumentationSystem.bootstrap(factory)
+        }
+    }
+
+    @Test func testBootstrapMetricsBackend() async throws {
+        // Bootstrapping once succeeds.
+        await #expect(processExitsWith: .success, "Running in a separate process because test uses bootstrap") {
+            var config = OTel.Configuration.default
+            config.logs.enabled = false
+            config.metrics.enabled = true
+            config.traces.enabled = false
+            _ = try OTel.bootstrap(configuration: config)
+        }
+        // We test the bootstrap API actually did bootstrap, by attempting a second bootstrap.
+        await #expect(processExitsWith: .failure, "Running in a separate process because test uses bootstrap") {
+            var config = OTel.Configuration.default
+            config.logs.enabled = false
+            config.metrics.enabled = true
+            config.traces.enabled = false
+            _ = try OTel.bootstrap(configuration: config)
+            MetricsSystem.bootstrap(NOOPMetricsHandler.instance)
+        }
+    }
+
+    @Test func testBootstrapTracingBackend() async throws {
+        // Bootstrapping once succeeds.
+        await #expect(processExitsWith: .success, "Running in a separate process because test uses bootstrap") {
+            var config = OTel.Configuration.default
+            config.logs.enabled = false
+            config.metrics.enabled = false
+            config.traces.enabled = true
+            _ = try OTel.bootstrap(configuration: config)
+        }
+        // We test the bootstrap API actually did bootstrap, by attempting a second bootstrap.
+        await #expect(processExitsWith: .failure, "Running in a separate process because test uses bootstrap") {
+            var config = OTel.Configuration.default
+            config.logs.enabled = false
+            config.metrics.enabled = false
+            config.traces.enabled = true
+            _ = try OTel.bootstrap(configuration: config)
+            InstrumentationSystem.bootstrap(NoOpTracer())
+        }
+    }
+}
+#endif // compiler(>=6.2)
